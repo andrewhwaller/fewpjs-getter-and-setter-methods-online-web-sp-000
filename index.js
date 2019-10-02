a08cecbc70468a5d6832818cd5c55769e3f9428b
Andrew Waller <andrewhwaller@gmail.com>
2019-10-02 00:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ class Cirle {
   }
 
   get circumference() {
-    return (this.radius * 2) * pi
+    return ( this.radius * 2 ) * pi
   }
 
   set circumference(circumference) {
-    this.radius = circumference/(pi * 2)
+    this.radius = circumference/( pi * 2 )
   }
 
   get area() {
-    return pi * (this.radius * this.radius)
+    return pi * ( this.radius * this.radius )
   }
 
   set area(area) {
-    this.radius = Math.sqrt(area/pi)
+    this.radius = Math.sqrt( area / pi )
   }
 }
